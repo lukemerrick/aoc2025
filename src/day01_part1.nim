4e7dd3a5c99f9ac5434a001f0d0b22b523d6d703
Luke Merrick <luke@datologyai.com>
2025-12-08 10:03:50 -0800

    Day 01

---
+++ src/day01_part1.nim
@@ -0,0 +1,27 @@
+import std/strformat
+import std/strutils
+import std/paths
+import std/math
+
+const
+  Day = 1
+  Part = 1
+  InputPath = Path(instantiationInfo(-1).filename).splitFile().dir / Path("inputs")
+  InputFile = InputPath / Path(&"day{Day:02}.txt")
+
+proc solve(input: string): int =
+  var result = 0
+  var position = 50
+  for line in input.strip().splitLines():
+    let direction = (if line[0] == 'R': 1 else: -1)
+    let distance = line[1..^1].parseInt()
+    let value = direction * distance
+    position = (100 + position + value) mod 100
+    if position == 0:
+      result += 1
+  return result
+
+when isMainModule:
+  let data = readFile($InputFile)
+  let answer = solve(data)
+  echo fmt"Day {Day} Part {Part}: {answer}"
---
+++ src/day01_part2.nim
@@ -0,0 +1,27 @@
+import std/strformat
+import std/strutils
+import std/paths
+import std/math
+
+const
+  Day = 1
+  Part = 2
+  InputPath = Path(instantiationInfo(-1).filename).splitFile().dir / Path("inputs")
+  InputFile = InputPath / Path(&"day{Day:02}.txt")
+
+proc solve(input: string): int =
+  var result = 0
+  var position = 50
+  var quotient: int
+  for line in input.strip().splitLines():
+    let direction = (if line[0] == 'R': 1 else: -1)
+    let distance = line[1..^1].parseInt()
+    let value = direction * distance
+    (quotient, position) = divmod(100 + position + value, 100)
+    result += abs(quotient - 1)
+  return result
+
+when isMainModule:
+  let data = readFile($InputFile)
+  let answer = solve(data)
+  echo fmt"Day {Day} Part {Part}: {answer}"
